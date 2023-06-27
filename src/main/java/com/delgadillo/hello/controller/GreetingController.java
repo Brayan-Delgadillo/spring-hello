4c702540b8e03f33022ca48e96ebc71426095eaf
Brayan-Delgadillo <brayan.delgadillo.a@tecsup.edu.pe>
2023-06-27 02:09:04 -0500

    Añadiendo servicio RESTful

---
+++ src/main/java/com/delgadillo/hello/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.delgadillo.hello.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.delgadillo.hello.model.Greeting; //importamos el modelo
+
+@RestController //anotacion para un controlador rest
+//devuelve JSON o XML
+
+public class GreetingController {
+	
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@GetMapping("/greeting") //declara que las solicitudes GET se dirigen a esta ruta | @RequestMappint(value = "/greeting", method=GET)
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
---
+++ src/main/java/com/delgadillo/hello/model/Greeting.java
@@ -0,0 +1,3 @@
+package com.delgadillo.hello.model;
+
+public record Greeting(long id, String content) { }
